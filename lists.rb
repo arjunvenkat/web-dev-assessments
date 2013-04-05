fdf8f30aa7030886211fdeb5d99db36ff6cdf598
Instructor <instructor@Arjuns-MacBook-Air.local>
2013-04-05 17:17:43 -0500

    added first versions of assessments for methods and lists

---
+++ lists.rb
@@ -0,0 +1,41 @@
+# Arrays
+
+# What are the two ways you can add "d" on to the end of the following array?
+         letters = ["a", "b", "c"]
+
+# How would you access the "b" in the following array?
+         letters = ["a", "b", "c"]
+
+# How would you access the "z" in the following array?
+         letters = ["a", "b",  ["x", "y", "z"]]
+
+# (optional): What am I doing wrong if I'm trying to access "x"
+          letters = ["a", "b",  ["x", "y", "z"]]
+          letters[2[0]]
+
+
+# Which is the hash and which is the array?
+   ["a", "b", "c"]           {"a" => 1, "b" => 2, "c" => 3}
+
+# How would you access the 2 in the following hash?
+   letter_hash = {"a" => 1, "b" => 2, "c" => 3}
+
+
+# Arrays are organized by ____________, hashes are organized by _____________
+
+# How would you access the "26" in the following hash?
+   letter_hash = {"a" => 1, "b" => {"x" => 24, "y" => 25, "z" => 26 }, "c" => 3}
+
+# How would you access Arjun's neighborhood, starting with the staff variable?
+staff = [
+   arjun = { "name" => {"first" => "Arjun", "last" => "Venkataswamy"}, "address" => {"neighborhood" => "Wicker Park", "zip" => 60622}}
+   raghu = { "name" => {"first" => "Raghu", "last" => "Betina"}, "address" => {"neighborhood" => "Lincoln Park", "zip" => 60614}}
+   jeff = { "name" => {"first" => "Jeff", "last" => "Cohen"}, "address" => {"neighborhood" => "Skokie", "zip" => 60077}}
+]
+
+
+# For the following lists, choose whether an array or a hash is more appropriate?
+   # 1. A list that you're going to be accessing by descriptive keywords
+   # 2. A list where order matters
+   # 3. A list that will eventually have an unknown number of elements
+
---
+++ methods.rb
@@ -0,0 +1,84 @@
+# Create a method called greeting that takes in no inputs and prints "Hello… you" to the screen
+#           example output) Hello…you
+
+
+# Create a method called personal_greeting that takes in an input and uses that input to print a personalized greeting
+#           example output) Hello Arjun
+
+
+# If you want the following script to print "hello" to the screen, what should be changed?
+     def print_hello
+          puts "hello"
+     end
+
+
+# What are the two ways I can call the following method?
+     def print_hello(name)
+          puts "hello " + name
+     end
+
+
+
+# If you want to the following script to print "hello Arjun" to the screen, what should be changed?
+     def print_hello(zebra)
+          puts "hello " + name
+     end
+
+     print_hello("Arjun")
+
+
+
+# If you want to the following script to print "hello Jeff Cohen" to the screen, what should be changed?
+     def print_hello(first_name, last_name)
+          puts "hello " + name
+     end
+
+
+     print_hello("Jeff", "Cohen")
+
+
+# If you want to the following script to print "hello Jeff Cohen" to the screen, what should be changed?
+     def print_hello(first_name, last_name)
+          puts "hello " + first_name + " " + last_name
+     end
+
+
+     print_hello ("Jeff", "Cohen")
+
+
+
+
+# What are the benefits to using a method in script 2 compared to script 1
+
+# script 1
+puts "Staff"
+puts "===="
+
+puts "Arjun is from Schaumburg. Arjun's favorite number is 3."
+puts "-------------------------------------------------------"
+puts # creates a new line
+
+puts "Raghu is from Goshen. Raghu's favorite number is 21."
+puts "----------------------------------------------------"
+puts
+
+puts "Jeff is from Skokie. Jeff's favorite number is 7."
+puts "-------------------------------------------------"
+puts
+
+
+
+# script 2
+def print_name(name, hometown, fav_num)
+     message = "#{name} is from #{hometown}. #{name}'s favorite number is #{fav_num}."
+     puts message
+     puts "-" * message.length
+     puts
+end
+
+puts "Staff"
+puts "===="
+
+print_name("Arjun", "Schaumburg", 3)
+print_name("Raghu", "Goshen", 21)
+print_name("Jeff", "Skokie", 7)
